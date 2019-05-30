0943b9960f67799b85bab03c6c1eef8fc7530ab6
dwnGnL <992938550845@yandex.ru>
2019-05-30 23:46:32 +0500

    func update

--- main.go
+++ main.go
@@ -4,25 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
-const (
-	Select = "select"
-	Create = "create"
-	Insert = "insert"
-	Delete = "delete"
-	Update = "update"
-)
+var ID int
+var db = map[string]*[]Student{}
+	var emptySlice = []Student{}
 
 func main() {
 	var login string
 	var password string
-	var ID int
 	var logined bool
-	db := map[string]*[]Student{}
-	var emptySlice = []Student{Student{}}
 	var exit bool
 
 	readedPassHash := readPasswordHash()
@@ -44,194 +35,9 @@ func main() {
 	for !exit {
 		reader := bufio.NewReader(os.Stdin)
 		command, _ := reader.ReadString('\n')
-		command = strings.TrimSpace(command)
-		command = strings.ToLower(command)
-		commandStruct := strings.Split(command, " ")
+		text:=allMake(command)
 
-		switch commandStruct[0] {
-		case Select:
-			tableName := strings.TrimSpace(commandStruct[3])
-			//tableName=strings.TrimSpace(tableName)
-
-			if db[tableName] == nil {
-				fmt.Print(tableName)
-				fmt.Println("table not exits")
-			} else {
-				columns := commandStruct[1]
-				switch columns {
-				case "*":
-					if len(commandStruct) > 4 {
-						if commandStruct[4] == "where" {
-							switch commandStruct[6] {
-							case "==":
-
-								switch commandStruct[5] {
-								case "id":
-
-									arrayOfStudents := *db[tableName]
-									fmt.Printf("  ID|               Fname|Age|Average| \n")
-									fmt.Println("------------------------------------")
-
-									id_num, _ := strconv.Atoi(commandStruct[7])
-									for _, row := range arrayOfStudents {
-
-										if row.ID == id_num {
-
-											fmt.Printf("%4d|%20s|%3d|%2.2f\n", row.ID, row.Fname, row.Age, row.Average)
-
-										}
-									}
-								}
-
-							}
-						} else {
-							fmt.Println(strings.Repeat("-", 50))
-							fmt.Println("| argument of command not recognize |")
-							fmt.Println(strings.Repeat("-", 50))
-						}
-					} else {
-						fmt.Printf("  ID|               Fname|Age|Average|Experience \n")
-						fmt.Println("------------------------------------")
-						arrayOfStudents := *db[tableName]
-
-						for _, row := range arrayOfStudents {
-
-							fmt.Printf("%4d|%20s|%3d|%4.2f|%2d\n", row.ID, row.Fname, row.Age, row.Average, row.Experience)
-						}
-						fmt.Println(strings.Repeat("-", 50))
-						fmt.Println("| rows returned: ", len(arrayOfStudents))
-						fmt.Println(strings.Repeat("-", 50))
-					}
-					break
-
-				default:
-					arrayOfStudents := *db[tableName]
-					colls := strings.Split(commandStruct[1], ",")
-					for _, row := range arrayOfStudents {
-						for _, coll := range colls {
-							coll = strings.ToLower(coll)
-							switch coll {
-							case "id":
-								// fmt.Printf("  ID|")
-								fmt.Printf("%4d|", row.ID)
-								break
-							case "fname":
-								// fmt.Printf("               Fname|")
-								fmt.Printf("%20s|", row.Fname)
-								break
-							case "age":
-								// fmt.Printf("Age|")
-								fmt.Printf("%3d|", row.Age)
-								break
-							case "average":
-								// fmt.Printf("Average ")
-								fmt.Printf("%2.2f", row.Average)
-								break
-							default:
-								fmt.Print("Ooops somefing wrong!")
-							}
-
-							// fmt.Printf("%4d|%20s|%3d|%2.2f\n", row.ID, row.Fname, row.Age, row.Average)
-						}
-						fmt.Println("")
-					}
-
-					break
-				}
-
-			}
-
-			break
-		case Create:
-			tableName := commandStruct[2]
-			db[tableName] = &emptySlice
-			emptySlice = nil
-			fmt.Println("table created: " + tableName)
-			break
-
-		case Insert:
-			args := commandStruct[1]
-			arg := strings.Split(args, ",")
-			tableName := commandStruct[3]
-			age, _ := strconv.Atoi(arg[1])
-			isStudent, _ := strconv.ParseBool(arg[2])
-			exp, _ := strconv.Atoi(arg[3])
-			if len(arg) == 4 {
-				if db[tableName] == nil {
-					fmt.Println("table not exits")
-				} else {
-					ID++
-					emptySlice = append(emptySlice, Student{
-						ID:         ID,
-						Age:        age,
-						Fname:      arg[0],
-						IsStudent:  isStudent,
-						Experience: exp,
-					})
-
-				}
-
-			} else {
-				fmt.Println(strings.Repeat("-", 50))
-				fmt.Println("| check value of coll or count of coll |")
-				fmt.Println(strings.Repeat("-", 50))
-			}
-			db[tableName] = &emptySlice
-
-			break
-
-		case Delete:
-			tableName := commandStruct[1]
-			emptySlice = *db[tableName]
-			if commandStruct[2] == "where" {
-
-				b, _ := strconv.Atoi(commandStruct[5])
-				for _, row := range emptySlice {
-					if row.ID == b {
-						emptySlice = append(emptySlice[:row.ID-1], emptySlice[row.ID:]...)
-					}
-				}
-				db[tableName] = &emptySlice
-
-			} else {
-				fmt.Println(strings.Repeat("-", 50))
-				fmt.Println("| check where argument |")
-				fmt.Println(strings.Repeat("-", 50))
-			}
-			break
-		case Update:
-			tableName := commandStruct[4]
-			arg := commandStruct[1]
-			emptySlice = *db[tableName]
-
-			b, _ := strconv.Atoi(commandStruct[8])
-
-			if arg == "*" {
-				args := commandStruct[2]
-				cols := strings.Split(args, ",")
-				age, _ := strconv.Atoi(cols[1])
-				isStudent, _ := strconv.ParseBool(cols[2])
-				exp, _ := strconv.Atoi(cols[3])
-				for _, row := range emptySlice {
-					if row.ID == b {
-
-						row.Age = age
-						row.Fname = cols[0]
-						row.IsStudent = isStudent
-						row.Experience = exp
-						emptySlice[row.ID-1] = row
-					}
-
-				}
-
-			}
-
-			break
-		default:
-			fmt.Println(strings.Repeat("-", 25))
-			fmt.Println("| command not recognize |")
-			fmt.Println(strings.Repeat("-", 25))
-		}
+		fmt.Println(text)
 	}
 
 }
